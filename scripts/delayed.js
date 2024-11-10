edaa13b13714be95ba3fa7d3b59c530e1f8d167b
David Nuescheler <david@nuescheler.ch>
2024-11-10 12:21:42 -0700

    chore: remove child

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,4 +5,4 @@ const s = document.createElement('script');
 s.type = 'text/javascript';
 s.src = 'https://platform.swellcx.com/widget/js/chat.js#950172ca-dc68-42ca-9630-87f062f244ac';
 s.dataset.apiToken = '950172ca-dc68-42ca-9630-87f062f244ac';
-document.head.appendChild(s);
+document.head.append(s);
